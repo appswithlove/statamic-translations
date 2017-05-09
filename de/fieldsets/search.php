a048a1c697ae2e02a6f472509bd112372dd4183c
Rémy <git@rrelmy.ch>
2017-05-09 14:52:21 +0200

    initial german translations

---
+++ de/fieldsets/search.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'driver' => 'Treiber', // 'Driver'
+    'driver_instruct' => 'Der gewünschte Such-Service.', // 'The service that will be used for searches.'
+
+    'algolia_api_key' => 'Algolia Admin API Schlüssel', // 'Algolia Admin API Key'
+    'algolia_app_id' => 'Algolia Applikation ID', // 'Algolia Application ID'
+
+    'auto_index' => 'Automatische Such-Indexierung', // 'Search auto-index'
+    'auto_index_instruct' => 'Wenn die automatische Such-Indexierung eingeschaltet ist, wird diese bei neuem und geändertem Inhalt automatisch aktualisiert.', // 'Enabling this will automatically update the search index when content is updated.'
+
+    'default_index' => 'Standard Index', // 'Default index'
+    'default_index_instruct' => 'Der Name des Standard-Suchindex.', // 'The name of the default search index.'
+
+];
